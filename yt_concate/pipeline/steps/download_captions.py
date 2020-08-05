407ff6acf602d62e7125278fa1036ea4f2a25334
wen lin li <willie300300@yahoo.com.tw>
2020-08-05 17:01:22 +0800

    Successfully download caption and save video list to file

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -5,19 +5,30 @@ from pytube import YouTube
 from .step import Step
 from .step import StepException
 
-
+import time
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
+        start = time.time()
         for url in data:
-            source = YouTube(url)
-            en_caption = source.captions.get_by_language_code('en')
-            en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            print('downloading caption for', url)
+            if utils.caption_file_exists(url):
+                print('found existing caption file')
+                continue
 
-            print(en_caption_convert_to_srt)
-            # save the caption to a file named Output.txt
-            text_file = open(utils.get_caption_path(url), "w")
+
+            try:
+                source = YouTube(url)
+                en_caption = source.captions.get_by_language_code('en')
+                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            except (KeyError, AttributeError):
+                print('Error when downloading caption for', url)
+                continue
+
+            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
-            break
+
+        end = time.time()
+        print('took', end - start, 'seconds')
 
